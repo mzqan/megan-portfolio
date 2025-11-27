5b24e28b1a9c123dfa0d4d9e5476f539d3f35c5e
mzqan <123510256+mzqan@users.noreply.github.com>
2025-11-27 00:10:27 -0500

    - add welcome section - add nav bar - define default and accent colors - apply montserrat font

---
+++ app/components/sections/welcome.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+const Welcome = () => {
+    return (
+        <section id="home" className="flex flex-col w-full h-screen justify-center items-center bg-[url(/background_home.png)] bg-cover bg-bottom bg-no-repeat">
+            <div className="flex justify-center md:w-3/5">
+                <motion.div
+                    className="w-full"
+                    initial={{ opacity: 0, y: -10 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.3 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                >
+                    <img
+                        src={"/welcome_banner.png"}
+                        draggable="false"
+                    />
+                </motion.div>
+            </div>
+        </section>
+    )
+}
+
+export default Welcome
---
+++ app/components/ui/nav.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const navLinks = [
+  { name: "home", href: "#home" },
+  { name: "about", href: "#about" },
+  { name: "experiences", href: "#experience" },
+];
+
+const Nav = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 450)
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [])
+
+  const scrollToSection = (href: string) => {
+    const element = document.querySelector(href);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
+  return (
+    <nav
+      className={`
+        fixed top-4 left-1/2 -translate-x-1/2 z-50 w-11/12 md:w-1/3 transition-all duration-300
+        ${
+          scrolled
+            ? "rounded-full backdrop-filter backdrop-blur-sm border-white shadow-lg"
+            : ""
+        }
+      `}
+    >
+      <div className="flex justify-center items-center w-full mx-auto px-3 py-3">
+        <div className="flex space-x-4 md:space-x-6 lg:space-x-8">
+          {navLinks.map((link) => (
+            <button
+              key={link.name}
+              className="text-lg font-bold text-primary hover:text-accent-primary transition-colors"
+              onClick={() => scrollToSection(link.href)}
+            >
+              {link.name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav
